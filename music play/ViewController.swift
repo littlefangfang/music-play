9d1a2ba5ea8cbd5a9aefdd83c1057b5535830a79
xinyue-0 <xinyue-0@aca80232.ipt.aol.com>
2017-01-10 14:38:52 +0800

    Initial Commit

---
+++ music play/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  music play
+//
+//  Created by xinyue-0 on 2017/1/10.
+//  Copyright © 2017年 xinyue-0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ music playTests/music_playTests.swift
@@ -0,0 +1,36 @@
+//
+//  music_playTests.swift
+//  music playTests
+//
+//  Created by xinyue-0 on 2017/1/10.
+//  Copyright © 2017年 xinyue-0. All rights reserved.
+//
+
+import XCTest
+@testable import music_play
+
+class music_playTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
